2749bd70dae8313d11d9a1539fafb1fff734471d
Esteban Herrera <eh@MacBook-Pro-de-Esteban.local>
2017-07-16 22:54:39 -0500

    bug fixes

--- winecoreml/ViewController.swift
+++ winecoreml/ViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var alcoholLabel: UILabel!
     @IBOutlet weak var alcoholSlider: UISlider!
     @IBOutlet weak var malicAcidLabel: UILabel!
-    @IBOutlet weak var malicAcidSlide: UISlider!
+    @IBOutlet weak var malicAcidSlider: UISlider!
     @IBOutlet weak var ashLabel: UILabel!
-    @IBOutlet weak var ashSlide: UISlider!
-    @IBOutlet weak var alcalinityAshLabel: UILabel!
-    @IBOutlet weak var alcalinityAshSlide: UISlider!
+    @IBOutlet weak var ashSlider: UISlider!
+    @IBOutlet weak var alkalinityAshLabel: UILabel!
+    @IBOutlet weak var alkalinityAshSlider: UISlider!
     @IBOutlet weak var magnesiumLabel: UILabel!
     @IBOutlet weak var magnesiumSlider: UISlider!
     @IBOutlet weak var totalPhenolsLabel: UILabel!
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stackView.setCustomSpacing(25, after: totalPhenolsSlider)
+        stackView.setCustomSpacing(30, after: totalPhenolsSlider)
         
         numberFormatter.numberStyle = .decimal
         numberFormatter.maximumFractionDigits = 2
@@ -44,7 +44,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func updateValues() {
         DispatchQueue.main.async {
             self.updateLabels();
@@ -52,22 +51,21 @@ class ViewController: UIViewController {
         }
     }
     
+    
     func updateLabels() {
         alcoholLabel.text = "Alcohol: \(numberFormatter.string(for: alcoholSlider.value) ?? "0")"
-        malicAcidLabel.text = "Malic Acid: \(numberFormatter.string(for: malicAcidSlide.value) ?? "0")"
-        ashLabel.text = "Ash: \(numberFormatter.string(for: ashSlide.value) ?? "0")"
-        alcalinityAshLabel.text = "Alcalinity Ash: \(numberFormatter.string(for: alcalinityAshSlide.value) ?? "0")"
+        malicAcidLabel.text = "Malic Acid: \(numberFormatter.string(for: malicAcidSlider.value) ?? "0")"
+        ashLabel.text = "Ash: \(numberFormatter.string(for: ashSlider.value) ?? "0")"
+        alkalinityAshLabel.text = "Alkalinity Ash: \(numberFormatter.string(for: alkalinityAshSlider.value) ?? "0")"
         magnesiumLabel.text = "Magnesium: \(numberFormatter.string(for: magnesiumSlider.value) ?? "0")"
         totalPhenolsLabel.text = "Total Phenols: \(numberFormatter.string(for: totalPhenolsSlider.value) ?? "0")"
-        
     }
     
+    
     func predictRegion() {
-        if let prediction = try? model.prediction(alcohol: Double(alcoholSlider.value), malicAcid: Double(malicAcidSlide.value), ash: Double(ashSlide.value), alcalinityAsh: Double(alcalinityAshSlide.value), magnesium: Double(magnesiumSlider.value), totalPhenols: Double(totalPhenolsSlider.value)) {
+        if let prediction = try? model.prediction(alcohol: Double(alcoholSlider.value), malicAcid: Double(malicAcidSlider.value), ash: Double(ashSlider.value), alkalinityAsh: Double(alkalinityAshSlider.value), magnesium: Double(magnesiumSlider.value), totalPhenols: Double(totalPhenolsSlider.value)) {
             regionLabel.text = "Region \(prediction.classLabel) (\(prediction.classProbability[prediction.classLabel] ?? 0))"
         }
-        
-        
     }
 }
 
